61cd9d4233ee1cc2d6eba2464f97471c32238332
Charles Peace <charlespeace@hotmail.com>
2022-04-07 17:25:24 -0700

    created connection to database and created inquirer prompt

---
+++ db/index.js
@@ -0,0 +1,54 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect();
+
+const { prompt } = require('inquirer');
+
+function promptUser () {
+    prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'Welcome to your employee manager tool! What would you like to do?',
+            choices: [
+                {
+                    name: 'View all departments',
+                    value: 'VIEW_DEPARTMENTS'
+                },
+                {
+                    name: 'View all roles',
+                    value: 'VIEW_ROLES'
+                },
+                {
+                    name: 'View all employees',
+                    value: 'VIEW_EMPLOYEES'
+                },
+                {
+                    name: 'Add a department',
+                    value: 'ADD_DEPARTMENT'
+                },
+                {
+                    name: 'Add a role',
+                    value: 'ADD_ROLE'
+                },
+                {
+                    name: 'Add an employee',
+                    value: 'ADD_EMPLOYEE'
+                },
+                {
+                    name: 'Update an employee role',
+                    value: 'UPDATE_EMPLOYEE_ROLE'
+                }
+            ]
+        }
+    ])
+};
+
+promptUser();
